56b4d0725934374a65eafc28e52d9d6d81dd1623
harshadfriend <harshad.friend@gmail.com>
2018-09-27 16:29:06 +0530

    commit

--- app/src/main/java/karjatonline/schoolbustrack/MapsActivity.java
+++ app/src/main/java/karjatonline/schoolbustrack/MapsActivity.java
@@ -3,11 +3,14 @@ package karjatonline.schoolbustrack;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.CountDownTimer;
+import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,10 +19,30 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     Button btnS;
+    String url = "https://wwwkarjatonlinecom.000webhostapp.com/add.php";
+    String url2 = "https://wwwkarjatonlinecom.000webhostapp.com/getdata.php";
+
+    StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +55,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onClick(View v) {
 
+                ArrayList nameValuePairs = new ArrayList();
+
+                nameValuePairs.add(new BasicNameValuePair("name","lat lang"));
+
+                nameValuePairs.add(new BasicNameValuePair("mobile","");
+
+
+//        Log.d(“well2”, “msg”);
+                StrictMode.setThreadPolicy(policy);
+
+//        Log.d(“well3”, “msg”);
+//http post
+                try{
+                    HttpClient httpclient = new DefaultHttpClient();
+
+                    HttpPost httppost = new HttpPost(url);
+
+                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    HttpResponse response = httpclient.execute(httppost);
+                    HttpEntity entity = response.getEntity();
+                    InputStream is = entity.getContent();
+//            Log.d(“well4”, “msg”);
+
+//            Log.e(“log_tag”, “connection success “);
+                    //  Toast.makeText(getApplicationContext(), "Please Wait….", Toast.LENGTH_SHORT).show();
+                }
+
+                catch(Exception e)
+                {
+//            Log.e(“log_tag”, “Error in http connection “+e.toString());
+                    Toast.makeText(getApplicationContext(), "Connection fail", Toast.LENGTH_SHORT).show();
+//            Log.d(“well5”, “msg”);
+
+                }
+
+                getJSON(url2);
+
                 new CountDownTimer(10000,1000){
                     @Override
                     public void onFinish() {
@@ -42,7 +102,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     public void onTick(long millisUntilFinished) {
 
                     }
-                }.start();
+                };//.start();
 
                 /*LocationManager lm=(LocationManager)getSystemService(LOCATION_SERVICE);
                 LocationListener ll=new LocationListener() {
@@ -94,5 +154,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void mapChange(double x){
+        LatLng syd=new LatLng(-34,x);
+        mMap.addMarker(new MarkerOptions().position(syd).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(syd));
+    }
+
+    private void getJSON(final String urlWebService) {
+        /*
+         * As fetching the json string is a network operation
+         * And we cannot perform a network operation in main thread
+         * so we need an AsyncTask
+         * The constrains defined here are
+         * Void -> We are not passing anything
+         * Void -> Nothing at progress update as well
+         * String -> After completion it should return a string and it will be the json string
+         * */
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            //this method will be called before execution
+            //you can display a progress bar or something
+            //so that user can understand that he should wait
+            //as network operation may take some time
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            //this method will be called after execution
+            //so here we are displaying a toast with the json string
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                //           Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                //  tv.setText(s);
+
+
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+
+
+
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n");
+                    }
+
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
     }
 }
